c8fee48fb6f09cc84ce5cee09b3c86b8ae410abe
felipesoaresg <felisoaresg@gmail.com>
2025-11-08 22:54:46 -0300

    update spotify auth

--- api/spotifyAuth.ts
+++ api/spotifyAuth.ts
@@ -4,7 +4,6 @@ const clientId = '38f716182f0049cb95eb35e92c4cb8b8';
 const clientSecret = '3691d3bbad3e42138b276039953338ae';
 
 export async function getAccessToken(): Promise<string> {
-  // 👇 Usa btoa() para gerar a string base64
   const authString = btoa(`${clientId}:${clientSecret}`);
 
   const response = await axios.post(
