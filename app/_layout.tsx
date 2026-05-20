1d5f3a602127c17fe8f5c9809a7fcbb856c1a67d
felipesoaresg <felisoaresg@gmail.com>
2026-05-20 12:01:15 -0300

    update layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,48 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import * as Notifications from 'expo-notifications';
-import { Stack } from 'expo-router';
+import { router, Slot } from 'expo-router';
+import { useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const queryClient = new QueryClient();
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
-    shouldShowBanner: true,
-    shouldShowList: true,
     shouldShowAlert: true,
-    shouldPlaySound: false,
+    shouldPlaySound: true,
     shouldSetBadge: false,
+    shouldShowBanner: true, 
+    shouldShowList: true,
   }),
 });
 
 export default function RootLayout() {
+  useEffect(() => {
+    const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+      const idPedido = response.notification.request.content.data.idPedido;
+      if (idPedido) {
+        router.push("/(tabs)/queue");
+      }
+    });
+
+    return () => subscription.remove();
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <SafeAreaProvider>
-          <Stack 
-            screenOptions={{ 
-              headerShown: false,
-              contentStyle: { backgroundColor: '#0d0d0d' }
+          <SafeAreaView
+            style={{
+              flex: 1,
+              backgroundColor: '#0d0d0d',
             }}
+            edges={['top', 'bottom']}
           >
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          </Stack>
-
+            <Slot />
+          </SafeAreaView>
         </SafeAreaProvider>
       </GestureHandlerRootView>
     </QueryClientProvider>
